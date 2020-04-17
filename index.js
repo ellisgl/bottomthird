540f5a34da8f48dd7badc948907863f1da53c76f
ellisgl <xelliss@gmail.com>
2020-04-17 07:35:55 -0500

    Initial Commit.

---
+++ index.js
@@ -0,0 +1,112 @@
+const app      = require('express')();
+const http     = require('http').createServer(app);
+const node_ssh = require('node-ssh');
+const io       = require('socket.io')(http);
+
+let bandwidth     = {rx: [0, 0], tx: [0, 0]};
+let headLineIndex = 0;
+let headLines     = [
+    'OLD MAN YELLS AT CLOUD',
+    'ARE YOU A BART OR A MILHOUSE?',
+    'BUMBLEBEE MAN CAUGHT IN STING',
+    'Ketchup Truck Hits Hamburger Stand',
+    'Prendsident, Rock Star To Swap Wives',
+    'SPINNING NEWS PAPER INJURES PRINTER',
+    'YOU NEED A HEART TO LIVE',
+    'Devs sit down at the Stand Up.'
+];
+let ssh           = new node_ssh();
+let temp          = '0';
+
+// Web routes.
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/logo.png', (req, res) => {
+    res.sendFile(__dirname + '/logo.png');
+});
+
+// Connect to router.
+ssh.connect({
+                host: process.env.ROUTER_HOST,
+                username: process.env.ROUTER_USER,
+                password: process.env.ROUTER_PASS
+            }).then(function () {
+    console.log('Connected to router.');
+});
+
+// Get and transmit router rx/tx of WAN port.
+function getBandWidth() {
+    ssh.execCommand('cat /proc/net/dev|grep eth0').then(function (result) {
+        if (result.stdout) {
+            let res = result.stdout.replace(/  +/g, ' ').split(' ');
+            // Add to the back of the stack, and remove the 1st item. Also divide by 8 (bits to bytes?)
+            bandwidth.rx.push(res[1] / 8);
+            bandwidth.rx.shift();
+            bandwidth.tx.push(res[9] / 8);
+            bandwidth.tx.shift();
+
+            io.emit('bandwidth', bandwidth);
+        }
+    });
+}
+
+// Transmit and cycle through headlines.
+function getHeadline() {
+    io.emit('headline', headLines[headLineIndex]);
+
+    ++headLineIndex;
+
+    if (headLineIndex > (headLines.length - 1)) {
+        headLineIndex = 0;
+    }
+}
+
+// Get and transmit the router CPU temperature.
+function getTemp() {
+    ssh.execCommand('cat /proc/dmu/temperature').then(function (result) {
+        if (result.stdout) {
+            temp = result.stdout.split("\t:")[1].split("�")[0].trim();
+            io.emit('temp', temp + '°');
+        }
+    });
+}
+
+// Get and transmit the initial temp. Set to 1 second to allow for SSH to connect.
+setTimeout(function () {
+    getTemp();
+}, 1000);
+
+// Get and transmit bandwidth every 1 second.
+setInterval(function () {
+    getBandWidth();
+}, 1000);
+
+// Get and transmit headline every 10 seconds.
+setInterval(function () {
+    getHeadline();
+}, 10000);
+
+// Get and transmit temp every 1 minute.
+setInterval(function () {
+    getTemp();
+}, 60000);
+
+// Do stuff on websocket connection.
+io.on('connection', (socket) => {
+    console.log('user connected');
+
+    // Send the headline and temp.
+    io.emit('headline', headLines[headLineIndex]);
+    io.emit('temp', temp + '°');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+// Listen for connections.
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
